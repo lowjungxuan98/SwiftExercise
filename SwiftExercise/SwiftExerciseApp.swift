478b5242ea25522c25e928358f94608867bbc70d
lowjungxuan <lowjungxuan@gmail.com>
2022-11-12 09:30:09 +0800

    Initial Commit

---
+++ SwiftExercise/SwiftExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExerciseApp.swift
+//  SwiftExercise
+//
+//  Created by Low Jung Xuan on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
